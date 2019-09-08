4b05baf9de0dc336973451223ad7f6d7dfdc8b49
poolbooyer <ryou3018@gmail.com>
2019-09-08 22:27:55 +0900

    add function of getting file list

--- shell/renamer.sh
+++ shell/renamer.sh
@@ -2,9 +2,12 @@
 start=`date '+%s'`
 echo ${start}
 # Please write command under here.
+echo "Please input target files' path"
+read path_target
 
-
-
+flist=(`ls ./${path_target}`)
+echo "./${path_target} have these files (${#flist[*]})"
+echo ${flist[@]}
 fin=`date '+%s'`
 echo ${fin}
 
