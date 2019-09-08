1468e8a607216730e065e4fcdd6a3a0569398cb1
poolbooyer <ryou3018@gmail.com>
2019-09-08 23:32:52 +0900

    add function of copy and rename files

--- shell/renamer.sh
+++ shell/renamer.sh
@@ -1,14 +1,32 @@
-
-start=`date '+%s'`
-echo ${start}
-# Please write command under here.
 echo "Please input target files' path"
 read path_target
 
+start=`date '+%s'`
+
+# Please write command under here.
+
 flist=(`ls ./${path_target}`)
 echo "./${path_target} have these files (${#flist[*]})"
-echo ${flist[@]}
+dir="copy"
+
+if [ -d ${dir} ]; then
+  echo "folder is created"
+else
+  mkdir ${dir}
+fi
+for item in ${flist[@]}; do
+    CP ./${path_target}/${item} ./${dir}/${item:3:3}doc.html >/dev/null 2>&1
+
+    if [ $?=0 ]; then
+      echo "${item} is success"
+    else
+      echo "${item} is error"
+    fi
+
+done
+
 fin=`date '+%s'`
+echo ${start}
 echo ${fin}
 
 echo $((fin - start))
